6b96aa6ac3b5bbea35d06d071986b198e36adf29
AlexPavy <alex.g.pavy@gmail.com>
2015-01-11 23:43:01 +0100

    Material works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,15 @@
 /* jshint devel:true */
 
-var app = angular.module('app', ['ngMaterial']);
+console.log("hello me")
+
+var app = angular.module('MainApp', ['ngMaterial']);
 app.controller('MainCtrl', function($scope) {
-    $scope.education = [];
+    $scope.education = [{
+        url: "https://www.ensta-paristech.fr/en",
+        desc: "MS Engineering school"
+    }];
+    $scope.experience = [{
+        url: "http://www.lesoffrescanal.fr/",
+        desc: "Le cube canal +"
+    }];
 });
